c2bfce052882ebf729b61ddbfa908ff20a5d4f1e
mayrmartin <martin.mayr98@gmail.com>
2017-01-05 18:15:06 +0100

    Add files via upload

---
+++ dailyBackground.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#This script pulls the daily wallpaper from bing.com and sets it as background.
+#If the PC is offline, the script will use the last downloaded image.
+
+
+market="en-US" #The available regions are: en-US,zh-CN,ja-JP,en-AU,en-UK,de-DE,en-NZ.
+resolution="1920x1080" #The aviable resolutions are: 1366×768, 1920×1080, 1920×1200.
+imageFileName=".wallpaper.jpg"
+downloadDirectory="$HOME"
+
+
+
+wget -q --tries=2 --timeout=5 --spider http://google.com
+if [[ $? -eq 0 ]]; then
+        xml=$(curl -k --silent "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=$market")
+	baseURL=$(grep -oPm1 "(?<=<url>)[^<]+" <<< "$xml")
+	
+	imageLink=$(echo "$baseURL" | sed -n "s/1366x768/$resolution/gp")
+
+	wget https://www.bing.com/$imageLink -O $downloadDirectory/$imageFileName
+	gsettings set org.gnome.desktop.background picture-uri file:///$downloadDirectory/$imageFileName
+else
+       gsettings set org.gnome.desktop.background picture-uri file:///$downloadDirectory/$imageFileName
+fi
+
+
+
